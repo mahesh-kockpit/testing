11ec850eab7e47c2f7d70fb7f0d975bb216d7294
mahesh-kockpit <mahesh.singh@kockpit.in>
2020-11-18 13:49:21 +0530

    new changes

--- test1.py
+++ test1.py
@@ -3,8 +3,3 @@ from pyspark import SparkContext
 sc =SparkContext()
 print('##############################')
 print(sc.parallelize([1,2,3,4]).collect())
-print('a')
-print('b')
-print('c')
-
-print('asda')
